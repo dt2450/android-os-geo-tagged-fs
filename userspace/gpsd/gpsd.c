81da18cf4a0c83d7036089f7094cb65f026d3c4b
Pratyush Parimal <pp2485@columbia.edu>
2014-11-28 20:07:05 -0500

    reading GPS values from file & sending to kernel, handling file errors.

--- userspace/gpsd/gpsd.c
+++ userspace/gpsd/gpsd.c
@@ -13,13 +13,57 @@
 struct gps_location *get_file_data()
 {
 	struct gps_location *loc = NULL;
+	FILE* f = NULL;
+	char line[128];
+	int line_num = 0;
+	double lat = -1, lon = -1;
+	float acc = -1;
+	int ret;
 
-	printf("gpsd: reading GPS data from file.\n");
-	//read from GPS_LOCATION_FILE here
 	loc = (struct gps_location *) malloc(sizeof(struct gps_location));
-	loc->latitude = 111;
-	loc->longitude = 222;
-	loc->accuracy = 333;
+	if (loc == NULL) {
+		perror("gpsd: couldn't allocate memory for location struct.");
+		return NULL;
+	}
+
+	//printf("gpsd: reading GPS data from file.\n");
+	f = fopen(GPS_LOCATION_FILE, "r");
+	if (f == NULL) {
+		perror("gpsd: couldn't open GPS file for reading.");
+		return NULL;
+	}
+
+	while (fgets(line, sizeof(line), f) != NULL) {
+		switch (line_num) {
+		case 0:	lat = strtod(line, NULL);
+			break;
+		case 1:	lon = strtod(line, NULL);
+			break;
+		case 2:	acc = strtof(line, NULL);
+			break;
+		default:printf("gpsd: unexpected GPS file format\n");
+			return NULL;
+		}
+		line_num++;
+	}
+
+	ret = fclose(f);
+	if (ret != 0) {
+		perror("gpsd: couldn't close GPS location file.");
+		return NULL;
+	}
+
+	if (lat == -1 || lon == -1 || acc == -1) {
+		printf("gpsd: error reading GPS file value(s).\n");
+		return NULL;
+	} else {
+		printf("gpsd: lat: %f, lon: %f, acc: %f\n",
+				lat, lon, acc);
+	}
+
+	loc->latitude = lat * 1000;
+	loc->longitude = lon * 1000;
+	loc->accuracy = acc * 1000;
 
 	return loc;
 }
@@ -34,7 +78,7 @@ int main(int argc, char *argv[])
 
 	pid = fork();
 	if (pid < 0) {
-		perror("gpsd: could not fork.\n");
+		perror("gpsd: could not fork.");
 		exit(EXIT_FAILURE);
 	}
 
@@ -43,7 +87,7 @@ int main(int argc, char *argv[])
 
 	sid = setsid();
 	if (sid < 0) {
-		perror("gpsd: could not create new session.\n");
+		perror("gpsd: could not create new session.");
 		exit(EXIT_FAILURE);
 	}
 
@@ -55,22 +99,22 @@ int main(int argc, char *argv[])
 	while(1) {
 		loc = get_file_data();
 		if (loc == NULL) {
-			perror("gpsd: could not read GPS data.\n");
+			printf("gpsd: could not read GPS data.\n");
 			exit(EXIT_FAILURE);
 		}
 
 		ret = set_gps_location(loc);
 		if (ret != 0) {
-			perror("gpsd: couldn't set GPS data in kernel.\n");
+			perror("gpsd: couldn't set GPS data in kernel.");
 			exit(EXIT_FAILURE);
 		}
+		free(loc);
 
 		if (usleep(1000 * 1000) == -1) {
-			perror("gpsd: couldn't sleep.\n");
+			perror("gpsd: couldn't sleep.");
 			exit(EXIT_FAILURE);
 		}
 	}
-
 
 	return 0;
 }
