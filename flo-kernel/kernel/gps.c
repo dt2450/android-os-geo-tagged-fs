ffdf3173fa2b323edf14191b5a52c34546907fb6
Pratyush Parimal <pp2485@columbia.edu>
2014-11-28 21:27:03 -0500

    persisting GPS data in static struct pointer

--- flo-kernel/kernel/gps.c
+++ flo-kernel/kernel/gps.c
@@ -8,10 +8,26 @@
 
 #include <linux/gps.h>
 
+static struct gps_location *__k_loc = NULL;
+
+static int init_k_loc()
+{
+	if (__k_loc == NULL) {
+		__k_loc = kmalloc(sizeof(struct gps_location), GFP_KERNEL);
+		if (__k_loc == NULL) {
+			pr_err("set_gps_location: couldn't allocate __k_loc\n");
+			return -ENOMEM;
+		}
+	}
+
+	return 1;
+}
+
 SYSCALL_DEFINE1(set_gps_location, struct gps_location __user *, loc)
 {
 	struct gps_location *k_loc = NULL;
 	int uid;
+	int ret;
 
 	read_lock(&tasklist_lock);
 	if (current == NULL || current->real_cred == NULL) {
@@ -33,6 +49,9 @@ SYSCALL_DEFINE1(set_gps_location, struct gps_location __user *, loc)
 		return -EINVAL;
 	}
 
+	if ((ret = init_k_loc()) != 1)
+		return ret;
+
 	k_loc = kmalloc(sizeof(struct gps_location), GFP_KERNEL);
 	if (k_loc == NULL) {
 		pr_err("set_gps_location: couldn't allocate k_loc\n");
@@ -43,6 +62,17 @@ SYSCALL_DEFINE1(set_gps_location, struct gps_location __user *, loc)
 		pr_err("set_gps_location: copy_from_user failed.\n");
 		return -EFAULT;
 	}
+
+	//maybe need to use locking here??
+	__k_loc->latitude = k_loc->latitude;
+	__k_loc->longitude = k_loc->longitude;
+	__k_loc->accuracy = k_loc->accuracy;
+	pr_err("GPS data: lat: %x, lon: %x, acc: %x\n",
+			__k_loc->latitude,
+			__k_loc->longitude,
+			__k_loc->accuracy);
+
+	kfree(k_loc);
 /*
 	//unable to do double and float related operations!!!
 
@@ -107,7 +137,7 @@ SYSCALL_DEFINE2(get_gps_location,
 			k_path, path_len);
 
 
-	pr_err("set_gps_location: lat: %x, long: %x, acc: %x\n",
+	pr_err("get_gps_location: lat: %x, long: %x, acc: %x\n",
 			k_loc->latitude,
 			k_loc->longitude,
 			k_loc->accuracy);
